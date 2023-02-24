d43d53a35906e6ec1f474676f3dbb93e82553a71
rushiauti23 <111104928+rushiauti23@users.noreply.github.com>
2023-02-24 18:01:38 +0530

    added the fullpage Animation files

---
+++ app.js
@@ -0,0 +1,20 @@
+new fullpage('#fullpage',{
+    autoScrolling: true,
+    navigation: true,
+    onLeave: (origin, destination, direction) =>{
+        const section = destination.item;
+        const title = section.querySelector('h1');
+        const tl = new TimelineMax({delay: 0.5});
+        tl.fromTo(title, 0.5, {y: '50', opacity:0}, {y: 0, opacity:1});
+
+        if(destination.index===1){
+            const chair = document.querySelectorAll('.chair');
+            const description = document.querySelector('.description');
+
+            tl.fromTo(chair, 0.7, {x:'100%'},{x:'-35%'})
+            .fromTo(chair[0],1,{opacity:1},{opacity:1})
+            .fromTo(chair[1],1,{opacity:0},{opacity:1})
+            .fromTo(chair[2],1,{opacity:0},{opacity:1})
+        }
+    }
+});
